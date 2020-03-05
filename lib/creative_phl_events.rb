92b1b86b88c6018a8f1920be8ecdaafe562d4040
d-otis <dan@dan-foley.com>
2020-03-05 16:40:37 -0500

    first commit

---
+++ lib/creative_phl_events.rb
@@ -0,0 +1,6 @@
+require "creative_phl_events/version"
+
+module CreativePhlEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/creative_phl_events/version.rb
@@ -0,0 +1,3 @@
+module CreativePhlEvents
+  VERSION = "0.1.0"
+end
