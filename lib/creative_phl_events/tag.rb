606fc9480b0f65287eca88cdcb1654ca4faf73f6
d-otis <dan@dan-foley.com>
2020-03-06 14:16:10 -0500

    implement find_or_create_by_tag

--- lib/creative_phl_events/tag.rb
+++ lib/creative_phl_events/tag.rb
@@ -1,10 +1,27 @@
-class Tag
-	attr_accessor :name
+class CreativePhlEvents::Tag
+	attr_accessor :name, :events
 
 	@@all = []
 
-	def initialize(name, event)
-		
+	def initialize
+		save
+		@events = []
+	end
+
+	def find_or_create_by_tag(tag, event)
+		if self.all.find {|tag_instance| tag_instance.name == tag}
+			self.all.find {|tag_instance| tag_instance.name == tag}.events << event
+			self.all.find {|tag_instance| tag_instance.name == tag}
+		else
+			new_tag = self.new
+			new_tag.name = tag
+			new_tag.events << event
+			new_tag
+		end
+	end
+
+	def save
+		self.all << self
 	end
 
 	def self.all
